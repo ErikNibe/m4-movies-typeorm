fe997eede55b5408e40c1e55cd7c487a58653713
Érik Eiji <eriknibe1@gmail.com>
2023-02-22 16:17:03 -0300

    fix

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,12 +3,14 @@ import "reflect-metadata";
 import { DataSource, DataSourceOptions } from "typeorm";
 import path from "path";
 
-
+// yarn typeorm migration:generate -d src/data-source.ts src/migrations/createTableMovies
 const dataSourceConfig = (): DataSourceOptions => {
 
     const migrationsPath: string = path.join(__dirname, "./migrations/**.{ts,js}");
     const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
 
+    console.log(migrationsPath)
+    console.log(entitiesPath);
     const dbUrl: string | undefined = process.env.DATABASE_URL;
 
     if (!dbUrl) throw new Error("Env var DATABASE_URL does not exists");
